9bba1bf1bcbd12b11355f139bb84de80a725e753
astaphobia <ariasta.ketut@icloud.com>
2018-09-04 12:10:50 +0800

    🍕 redux store and routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './favicon.ico'
+import Routes from './routes'
 
-const App = () => (
-    <div className="container">
-        <h1>Hello</h1>
-    </div>
-)
-
+const Store = createStore(() => { }, {}, applyMiddleware(reduxThunk))
 ReactDOM.render(
-    <App />,
+    <Provider store={Store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 )
